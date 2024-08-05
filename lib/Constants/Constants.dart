c18751ecd00c1a786a29fb79b5f20b83bc7dd6b9
Jason Park <142364450+DutchVandaline@users.noreply.github.com>
2024-08-06 01:07:51 +0900

    Update Constants.dart

--- lib/Constants/Constants.dart
+++ lib/Constants/Constants.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
 class Constants {
-  static const String apikey = "AIzaSyDKWLYNlzIHPPmp3M7LNxVruDvRbF-pKIo";
+  static const String apikey = "";
   static const String newsLine = 'DEADLINE Your animal is suffering '
       'but that\'s totally fine! There are 2.2 million animal species left. '
       'Feeling Uncomfortable? You will be more uncomfortable if you don\'t act now! ';
